616c7c868998a97158fdd364ee86b246c595ce95
Mark Wylde <mark@wylde.net>
2026-09-22 22:30:27 +0100

    Revert the retrying test runner; fix flaky tests at the source instead

--- scripts/failed-tests-reporter.mjs
+++
@@ -1,9 +0,0 @@
-export default async function* failedTests(source) {
-	const failed = [];
-	for await (const event of source) {
-		if (event.type === 'test:fail' && event.data.nesting === 0) {
-			failed.push({ file: event.data.file, name: event.data.name });
-		}
-	}
-	yield JSON.stringify(failed);
-}
